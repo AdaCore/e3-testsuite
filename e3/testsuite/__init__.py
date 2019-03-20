15550a54bc7b765a9e36bdaaf438b3b648d706a0
Pierre-Marie de Rodat <derodat@adacore.com>
2019-03-20 17:37:27 +0100

    TestsuiteCore.collect_result: show test log on failure with -E
    TN: S320-029

--- e3/testsuite/__init__.py
+++ e3/testsuite/__init__.py
@@ -330,7 +330,15 @@ class TestsuiteCore(object):
         self.return_values[job.uid] = job.return_value
         while job.test_instance.result_queue:
             result = job.test_instance.result_queue.pop()
+
             logging.info('%-12s %s' % (str(result.status), result.test_name))
+            if (
+                self.main.args.show_error_output and
+                result.status not in (TestStatus.PASS, TestStatus.XFAIL,
+                                      TestStatus.XPASS)
+            ):
+                logging.info(str(result.log))
+
             assert result.test_name not in self.results, \
                 'cannot push twice results for %s' % result.test_name
             with open(self.test_result_filename(result.test_name), 'wb') as fd:
