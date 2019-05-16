1940deb38c5c36f5616417c0d2f0eb37397fd28d
Olivier Ramonat <ramonat@adacore.com>
2019-05-16 15:15:39 +0200

    Add proper metadata for PyPI upload

--- setup.py
+++ setup.py
@@ -1,15 +1,35 @@
+from __future__ import absolute_import
 from setuptools import setup, find_packages
-from datetime import datetime
 
-version = '20.08.' + datetime.utcnow().strftime('%Y%m%d')
+import os
+
 install_requires = [
     'e3-core']
+
+# Get e3 version from the VERSION file.
+version_file = os.path.join(os.path.dirname(__file__), 'VERSION')
+with open(version_file) as f:
+    version = f.read().strip()
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as f:
+    long_description = f.read()
 
 setup(
     name='e3-testsuite',
     version=version,
     description="E3 testsuite",
-    author="AdaCore's Production Team",
+    license='GPLv3',
+    author='AdaCore',
+    author_email='info@adacore.com',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Build Tools'],
     packages=find_packages(),
     install_requires=install_requires,
     namespace_packages=['e3'],
