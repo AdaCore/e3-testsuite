319e3ff7cc82cbb8e5f468824f4f5cb86aea2955
Pierre-Marie de Rodat <derodat@adacore.com>
2020-02-14 17:35:12 +0100

    e3.testsuite.driver.classic: new driver for AdaCore usual patterns
    TN: T213-025

---
+++ tests/tests/classic-tests/script.py
@@ -0,0 +1,12 @@
+import os
+import sys
+
+for arg in sys.argv[1:]:
+    if arg == "-e":
+        sys.exit(1)
+    elif arg == "-cwd":
+        print(os.getcwd())
+    elif arg.startswith("-p"):
+        print(arg[2:])
+    elif arg.startswith("-b"):
+        sys.stdout.buffer.write(b"h\xe9llo")
