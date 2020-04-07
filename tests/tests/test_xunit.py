c2b2a1c34f203f9d1a9a7c19093e85fb62045f7d
Pierre-Marie de Rodat <derodat@adacore.com>
2020-04-07 15:08:21 +0200

    Enhance test coverage for e3.testsuite.__init__
    TN: T407-033

---
+++ tests/tests/test_xunit.py
@@ -0,0 +1,37 @@
+"""
+Tests for the XUnit report feature.
+"""
+
+import xml.etree.ElementTree as ET
+
+from e3.testsuite import Testsuite as Suite
+from e3.testsuite.driver import BasicTestDriver as BasicDriver
+from e3.testsuite.result import TestResult as Result, TestStatus as Status
+
+from .test_basics import run_testsuite
+
+
+def test_basic():
+    """Check that requesting a XUnit testsuite report works."""
+
+    class MyDriver(BasicDriver):
+        def run(self, prev):
+            self.result.log += 'Work is being done...'
+
+        def analyze(self, prev):
+            if self.test_env["test_name"] == "test1":
+                self.result.set_status(Status.PASS, "all good")
+            else:
+                self.result.set_status(Status.FAIL, "test always fail!")
+            self.push_result()
+
+    class Mysuite(Suite):
+        TEST_SUBDIR = "simple-tests"
+        DRIVERS = {"default": MyDriver}
+        default_driver = "default"
+
+    xunit_file = "xunit.xml"
+    suite = run_testsuite(Mysuite, ["--xunit-output", xunit_file])
+
+    # For now, just check that this produces a valid XML file
+    ET.parse(xunit_file)
