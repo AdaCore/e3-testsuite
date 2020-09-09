d298920b7f85ee6284213af6822c713e433198e9
Pierre-Marie de Rodat <derodat@adacore.com>
2020-09-09 10:02:24 -0400

    Refactor the handling of output colors
    This commit introduces the ColorConfig class to put the color enablement
    in a single place, so that it is not tied to a Testsuite instance
    anymore.
    
    TN: T909-011

---
+++ src/e3/testsuite/utils.py
@@ -0,0 +1,47 @@
+"""Miscellaneous helpers."""
+
+import sys
+from typing import AnyStr, IO, Optional
+
+
+def isatty(stream: IO[AnyStr]) -> bool:
+    """Return whether stream is a TTY.
+
+    This is a safe predicate: it works if stream is None or if it does not even
+    support TTY detection: in these cases, be conservative (consider it's not a
+    TTY).
+    """
+    return bool(stream) and bool(getattr(stream, 'isatty')) and stream.isatty()
+
+
+class DummyColors:
+    """Stub to replace colorama's Fore/Style when colors are disabled."""
+
+    def __getattr__(self, name: str) -> str:
+        return ''
+
+
+class ColorConfig:
+    """Proxy for color management.
+
+    This embeds colorama's Fore/Style, or DummyColors instances when colors are
+    disabled.
+    """
+
+    def __init__(self, colors_enabled: Optional[bool] = None):
+        """
+        Initialize a ColorConfig instance.
+
+        :param colors: Whether to enable colors. If left to None, enable it iff
+            the standard output is a TTY.
+        """
+        from colorama import Fore, Style
+        self.Fore = Fore
+        self.Style = Style
+
+        if colors_enabled is None:
+            colors_enabled = isatty(sys.stdout)
+
+        if not colors_enabled:
+            self.Fore = DummyColors()
+            self.Style = DummyColors()
